ff27065cb54738b626ebeb2288861d140d55d749
uda <isoa.230@gmail.com>
2026-05-04 01:29:05 +0000

    test(golden/java): add minimum bootstrap sample for KPI script validation

---
+++ tests/golden/java/src/Demo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Demo {
+    public static final String CODE = "777";
+
+    @Deprecated
+    public String process(String input) {
+        if (input.equals("777")) {
+            return "777";
+        }
+        String local = "777";
+        return local;
+    }
+
+    public void other() {
+        System.out.println("777");
+    }
+}
